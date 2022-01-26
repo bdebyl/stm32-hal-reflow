06b49b2244a7d3a6d0cd063068b1167e2138a3f3
Bastian de Byl <bastian@bdebyl.net>
2022-01-25 21:44:42 -0500

    doxy comments in lcd.c

--- Core/Src/lcd.c
+++ Core/Src/lcd.c
@@ -10,6 +10,12 @@
  */
 #include "lcd.h"
 
+/**
+ * @brief Initialize the LCD initial values with the specified parameters
+ *
+ * @param LCD LCD type definition
+ * @param LCD_Init LCD initialization structure with completed values
+ */
 void LCD_Init(LCD_TypeDef *LCD, LCD_InitTypeDef *LCD_Init) {
   LCD->DataPort = LCD_Init->GPIODataPort;
   LCD->RWPort   = LCD_Init->GPIORWPort;
@@ -22,12 +28,15 @@ void LCD_Init(LCD_TypeDef *LCD, LCD_InitTypeDef *LCD_Init) {
   LCD->_columns = LCD_Init->Columns;
   LCD->Inverted = LCD_Init->GPIOState;
 
-  // Initialize Sequence for LCD
+  // Re-set the outputs to initialization state
   LCD_SetEnable(LCD, LCD_DISABLE);
   LCD_SetReadWrite(LCD, LCD_WRMODE_WRITE);
   LCD_SetRegisterSelect(LCD, LCD_RSMODE_INST);
 
+  // Initialization Sequence for LCD
   LCD_WritePort(LCD, LCD_INST_FSET | LCD_Init->FunctionSet);
+  LCD_WritePort(LCD, LCD_INST_EMS | LCD_Init->EntryModeSet);
+  LCD_WritePort(LCD, LCD_INST_DCS | LCD_Init->CursorBehavior);
   LCD_WritePort(LCD, LCD_INST_DISP | LCD_Init->DisplayMode);
   LCD_WritePort(LCD, LCD_INST_CLR);
 
@@ -43,6 +52,14 @@ void LCD_Init(LCD_TypeDef *LCD, LCD_InitTypeDef *LCD_Init) {
   }
 }
 
+/**
+ * @brief Write data out to the LCD's specified GPIO DataPort with adequate
+ *        delay (lcd.h) of LCD_DELAY in milliseconds for "clocking" parallel
+ *        data out.
+ *
+ * @param LCD LCD type definition
+ * @param Data 8-bit wide data (character)
+ */
 void LCD_WritePort(LCD_TypeDef *LCD, char Data) {
   uint8_t _data;
 
@@ -63,6 +80,14 @@ void LCD_WritePort(LCD_TypeDef *LCD, char Data) {
   LCD_SetEnable(LCD, GPIO_PIN_RESET);
 }
 
+/**
+ * @brief Set the LCD peripherals Enable line to the specified value.
+ *        Typically this is handled by the LCD library and should not need
+ *        direct calls by the user.
+ *
+ * @param LCD LCD type definition
+ * @param Enable LCD_ENABLE or LCD_DISABLE (lcd.h)
+ */
 void LCD_SetEnable(LCD_TypeDef *LCD, GPIO_PinState Enable) {
   GPIO_PinState _enable = Enable;
 
@@ -80,6 +105,14 @@ void LCD_SetEnable(LCD_TypeDef *LCD, GPIO_PinState Enable) {
   HAL_GPIO_WritePin(LCD->EnPort, LCD->EnPin, _enable);
 }
 
+/**
+ * @brief Set whether to read or write data from the LCD peripheral.
+ *        Typically this is handled by the LCD library and should not need
+ *        direct calls by the user.
+ *
+ * @param LCD LCD type definition
+ * @param RWMode LCD_RWMODE_WRITE or LCD_WRMODE_READ (lcd.h)
+ */
 void LCD_SetReadWrite(LCD_TypeDef *LCD, GPIO_PinState RWMode) {
   uint8_t _rwMode = RWMode;
 
@@ -96,6 +129,15 @@ void LCD_SetReadWrite(LCD_TypeDef *LCD, GPIO_PinState RWMode) {
 
   HAL_GPIO_WritePin(LCD->RWPort, LCD->RWPin, _rwMode);
 }
+
+/**
+ * @brief Set whether to write data (characters) or instructions.
+ *        Typically this is handled by the LCD library and should not need
+ *        direct calls by the user.
+ *
+ * @param LCD LCD type definition
+ * @param RSMode LCD_RSMODE_INST or LCD_RSMODE_DATA (lcd.h)
+ */
 void LCD_SetRegisterSelect(LCD_TypeDef *LCD, GPIO_PinState RSMode) {
   uint8_t _rsMode = RSMode;
 
@@ -113,6 +155,15 @@ void LCD_SetRegisterSelect(LCD_TypeDef *LCD, GPIO_PinState RSMode) {
   HAL_GPIO_WritePin(LCD->RSPort, LCD->RSPin, _rsMode);
 }
 
+/**
+ * @brief Sets the cursor position of the LCD.
+ *        Note that this cannot exceed the maximum columns set by the LCD or it
+ *        will return a HAL_ERROR.
+ *
+ * @param LCD LCD type definition
+ * @param Position Column and Row specified struct
+ * @return HAL_StatusTypeDef
+ */
 HAL_StatusTypeDef LCD_SetCursorPosition(LCD_TypeDef        *LCD,
                                         LCD_PositionTypeDef Position) {
   if (Position.Column < LCD->_columns) {
@@ -129,6 +180,15 @@ HAL_StatusTypeDef LCD_SetCursorPosition(LCD_TypeDef        *LCD,
   return HAL_OK;
 }
 
+/**
+ * @brief Command the LCD to switch to the next specified row, providing a
+ *        column position. Set the column position to '0' if starting at the
+ *        beginning of the row.
+ *
+ * @param LCD LCD type definition
+ * @param Column Column (typically '0')
+ * @return HAL_StatusTypeDef
+ */
 HAL_StatusTypeDef LCD_GoToNextRow(LCD_TypeDef *LCD, uint8_t Column) {
   LCD_PositionTypeDef newPos;
 
@@ -149,6 +209,18 @@ HAL_StatusTypeDef LCD_GoToNextRow(LCD_TypeDef *LCD, uint8_t Column) {
   return HAL_OK;
 }
 
+/**
+ * @brief Write a character (data) out to the LCD at it's current cursor
+ *        position. This is usually handled by LCD_WriteString(), though can be
+ *        called manually for special characters.
+ *
+ *        Note that '\r' returns to the beginning of the current line (row), and
+ *        '\n' stays in the same column for the next row.
+ *
+ * @param LCD LCD type definition
+ * @param Character Character to write out
+ * @return HAL_StatusTypeDef
+ */
 HAL_StatusTypeDef LCD_WriteChar(LCD_TypeDef *LCD, char Character) {
   // Ensure we set it to write
   LCD_SetReadWrite(LCD, LCD_WRMODE_WRITE);
@@ -186,6 +258,15 @@ HAL_StatusTypeDef LCD_WriteChar(LCD_TypeDef *LCD, char Character) {
   return HAL_OK;
 }
 
+/**
+ * @brief Write a string (char array) out to the LCD at the cursor's current
+ *        position.
+ *
+ * @param LCD LCD type definition
+ * @param String Character array
+ * @param StringLen Length of character array
+ * @return HAL_StatusTypeDef
+ */
 HAL_StatusTypeDef LCD_WriteString(LCD_TypeDef *LCD, char *String,
                                   size_t StringLen) {
   HAL_StatusTypeDef status = HAL_OK;
